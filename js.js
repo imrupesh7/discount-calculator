e970a944637f2458859ded09c0a6bdf0cbee8a0c
Rupesh Jha <rupeshjha7796@gmail.com>
2024-04-10 22:31:07 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function checkValue() {
+    let x = parseFloat(document.getElementById("origiprice").value);
+    let z = parseFloat(document.getElementById("discountprice").value);
+
+    let sum = x / 100 * z;
+
+
+    let c = document.getElementById("savingamount").value = sum.toFixed();
+    let d = document.getElementById("discountamount").value = sum = x - z / 100 * x.toFixed();
+
+    let printvalue = `Your Saving Amount is ₹${c} Your Discount Amount is ${d}%`;
+    document.getElementById('value').innerHTML = printvalue;
+}
