d424cbc43635bcf8eeca2b6f811e5604534f6aa9
Rupesh Jha <rupeshjha7796@gmail.com>
2024-05-03 17:49:26 +0530

    Update js.js

--- js.js
+++ js.js
@@ -5,8 +5,8 @@ function checkValue() {
     let sum = x / 100 * z;
 
 
-    let c = document.getElementById("savingamount").value = sum.toFixed();
-    let d = document.getElementById("discountamount").value = sum = x - z / 100 * x.toFixed();
+    let c = document.getElementById("savingamount").value = Math.round(sum.toFixed());
+    let d = document.getElementById("discountamount").value = Math.round(sum = x - z / 100 * x.toFixed());
 
     let printvalue = `Your Saving Amount is ₹${c} Your Discount Amount is ${d}%`;
     document.getElementById('value').innerHTML = printvalue;
